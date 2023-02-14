a79130ffe8c040d5891d9c8838aa19c15bfbdaee
sadmankiba <sadmankiba@cs.wisc.edu>
2023-02-13 20:55:49 -0600

    Add dpdk setup

--- setup.sh
+++ setup.sh
@@ -11,3 +11,14 @@ rm MLNX_OFED_LINUX-4.9-5.1.0.0-ubuntu20.04-x86_64.tgz
 
 sudo /etc/init.d/openibd restart 
 ibv_devinfo
+
+git clone https://github.com/DPDK/dpdk
+cd dpdk
+git checkout releases
+meson build -Dexamples=all
+cd build
+meson configure
+ninja
+sudo ninja install
+sudo ldconfig
+echo 1024 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
