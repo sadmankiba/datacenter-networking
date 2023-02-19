27a1430cf1b7fa2d0f2ddb6b076ffc816a928f5b
sadmankiba <sadmankiba@cs.wisc.edu>
2023-02-18 20:01:54 -0700

    Add bw flow size

--- Client/bench.sh
+++ Client/bench.sh
@@ -2,18 +2,22 @@
 # 1GB/2GB/4GB/8GB/16GB/32GB 
 
 make clean
-make DEBUG=1
+make
 
-echo "Flow size, Latency (us), Bandwidth (Gbps)" > bench.out
-sizes="64 128 256 512 1024 $((2 * 1024)) $((4 * 1024)) \
+echo "Flow size, Latency (us), Bandwidth (Gbps)" > bench.csv
+lat_sizes="64 128 256 512 1024 $((2 * 1024)) $((4 * 1024)) \
     $((8 * 1024)) $((16 * 1024))"
-sms="64 128"
+bw_sizes="$((1 * 1024 * 1024 * 1024)) $((2 * 1024 * 1024 * 1024)) \
+    $((4 * 1024 * 1024 * 1024)) $((8 * 1024 * 1024 * 1024)) \
+    $((16 * 1024 * 1024 * 1024)) $((32 * 1024 * 1024 * 1024))"
 
-for s in $sizes; do
-    echo $s
-    echo -n "$s, " >> bench.out
-    export FLOW_SIZE=$s 
-    make run | tail -n 1 >> bench.out
-    echo "" >> bench.out
-    sleep 5
-done
+for sizes in "$lat_sizes" "$bw_sizes"; do 
+    for s in $sizes; do
+        echo $s
+        echo -n "$s, " >> bench.csv
+        export FLOW_SIZE=$s 
+        make run | tail -n 1 >> bench.csv
+        echo "" >> bench.csv
+        sleep 5
+    done
+done 
