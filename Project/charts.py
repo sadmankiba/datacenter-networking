13d9c594c4e123984b7e8771af6d488ae30f0f93
Sadman Sakib <sadmankiba@cs.wisc.edu>
2023-05-05 04:41:18 -0500

    Add storage 1-to-1 bw

--- Project/charts.py
+++ Project/charts.py
@@ -1,25 +1,65 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Write a matplotlib code to plot bar chart
-# for the following data:
-# 1	1105
-# 2	1588
-# 4	2680
-# 8	2880
-def main():
-    import matplotlib.pyplot as plt
-    import numpy as np
-    # Set labels for x axis
+def rem_storage_one_to_one_throughput():
+    x = range(1,5)
+    y1 = np.array([910, 1690 / 2, 2980 / 4, 2850 / 8])
+    y2 = np.array([910, 1690, 2980, 2850])
+    plt.xticks([1, 2, 3, 4], ['1', '2', '4', '8'])
+    plt.bar(x, y1, alpha=0.8, color='blue')
+    plt.bar(x,y2, alpha=0.5, color='red')
     plt.xlabel('Number of apps')
     plt.ylabel('Total throughput (MiBps)')
-    # Use x-ticks to set the values for x axis
-    # range 1- 4
-    x = range(1,5)
-    y = np.array([1105,1588,2680,2880])
-    # Set x-ticks values
-    plt.xticks([1, 2, 4, 8])
-    plt.bar(x,y)
     plt.show()
 
+def rem_storage_one_to_one_util():
+    x = np.arange(4)
+    y1 = np.array([40, 38, 39, 26])
+    y2 = np.array([37, 35, 32, 16])
+
+    # Plot bar chart of y1 and y2 side-by-side with x as x-axis 
+    plt.bar(x - 0.2, y1, color = 'b', width = 0.4)
+    plt.bar(x + 0.2, y2, color = 'g', width = 0.4)
+
+    # Add xticks on the middle of the group bars
+    plt.xlabel('Number of apps')
+    plt.ylabel('CPU Utilization (%)')
+    plt.xticks([0, 1, 2, 3], ['1', '2', '4', '8'])
+    # Set yticks
+    plt.yticks([10, 20, 30, 40, 50])
+
+    # Set legend
+    plt.legend(labels=['sender', 'receiver'])
+
+
+def storage_many_one_to_one_nw_one():
+    x = np.arange(4)
+    y1 = np.array([750, 701, 480, 263])
+    y2 = np.array([0, 940, 550, 285])
+
+    # Plot bar chart of y1 and y2 side-by-side with x as x-axis 
+    plt.bar(x - 0.2, y1, color = 'tab:blue', width = 0.4)
+    plt.bar(x + 0.2, y2, color = 'tab:orange', width = 0.4)
+
+    # Add xticks on the middle of the group bars
+    plt.xlabel('Number of storage T-apps')
+    plt.ylabel('T-app bandwidth (MBps)')
+    plt.xticks([0, 1, 2, 3], ['1', '2', '4', '8'])
+    # Set yticks
+    plt.yticks([200, 400, 600, 800, 1000])
+
+    # Set legend
+    plt.legend(labels=['in-contention', 'non-contention'])
+
+# Nw bandwidth in contention (Gbps)
+# 18.1
+# 16.6
+# 16.7
+# 17.5
+# 19.3
+def storage_many_one_to_one_nw_bw():
+    pass
+
 if __name__ == '__main__':
-    main()
+    storage_many_one_to_one_nw_one()
+    plt.show()
