ff01134b88add8e998f69ccec4b88f8a9b056304
Sadman Sakib <sadmankiba@cs.wisc.edu>
2023-05-10 19:31:31 -0600

    Add charts for presentation

--- Project/charts.py
+++ Project/charts.py
@@ -1,15 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
+# 1	1115
+# 2	1940
+# 4	2260
+# 8	2400
 
 def rem_storage_one_to_one_throughput():
     x = range(1,5)
-    y1 = np.array([910, 1690 / 2, 2980 / 4, 2850 / 8])
-    y2 = np.array([910, 1690, 2980, 2850])
+    y1 = np.array([1115, 1940 / 2, 2260 / 4, 2400 / 8])
+    y2 = np.array([1115, 1940, 2260, 2400])
     plt.xticks([1, 2, 3, 4], ['1', '2', '4', '8'])
-    plt.bar(x, y1, alpha=0.8, color='blue')
-    plt.bar(x,y2, alpha=0.5, color='red')
-    plt.xlabel('Number of apps')
-    plt.ylabel('Total throughput (MiBps)')
+    plt.bar(x, y1, color='tab:blue', width=0.6)
+    plt.bar(x, y2, alpha=0.5, color='cornflowerblue', width=0.6)
+    plt.xlabel('Number of T-apps')
+    plt.ylabel('Total throughput (MBps)')
 
 def rem_storage_one_to_one_util():
     x = np.arange(4)
@@ -17,11 +21,11 @@ def rem_storage_one_to_one_util():
     y2 = np.array([37, 35, 32, 16])
 
     # Plot bar chart of y1 and y2 side-by-side with x as x-axis 
-    plt.bar(x - 0.2, y1, color = 'b', width = 0.4)
-    plt.bar(x + 0.2, y2, color = 'g', width = 0.4)
+    plt.bar(x - 0.2, y1, color = 'tab:blue', width = 0.4)
+    plt.bar(x + 0.2, y2, color = 'tab:orange', width = 0.4)
 
     # Add xticks on the middle of the group bars
-    plt.xlabel('Number of apps')
+    plt.xlabel('Number of T-apps')
     plt.ylabel('CPU Utilization (%)')
     plt.xticks([0, 1, 2, 3], ['1', '2', '4', '8'])
     # Set yticks
@@ -90,8 +94,6 @@ def l_app_lat():
     y = np.array([61, 174, 230, 265, 310])
     # Add error bar on y
     yerr = np.array([0, 276 - 174, 350 - 230, 350 - 265, 430 - 310])
-    # Plot y as bar and yerror as a cap line
-    plt
     plt.errorbar(x, y, yerr=yerr, fmt='o', color='k', capsize=5, zorder=1)
     plt.xticks([0, 1, 2, 3, 4], ['isolated', '1', '2', '4', '8'])
     # draw the bar plot on top of the error bars
@@ -99,8 +101,92 @@ def l_app_lat():
     plt.xlabel('Number of L-apps')
     plt.ylabel('Latency (us)')
 
+
+def l_app_nw_one():
+    # 23.4
+    # 22.6
+    # 21.4
+    # 19.2
+    x = np.arange(4)
+    y = np.array([23.4, 22.6, 21.4, 19.2])
+    plt.xticks([0, 1, 2, 3], ['1', '2', '4', '8'])
+    plt.bar(x, y, color='tab:blue', width=0.5)
+    plt.xlabel('Number of L-apps')
+    plt.ylabel('Throughput (Gbps)')
+    # Plot a horizontal dashed line
+    plt.axhline(y=26.2, color='r', linestyle='--')
+    # Add a text above the horizontal line
+    plt.text(0.5, 26.2, 'isolated', ha='center', va='bottom', color='r')
+
+def l_app_nw_many_one_to_one():
+    # 1	310	430
+    # 2	340	490
+    # 3	1160	2780
+    # 4	1400	4000
+    x = np.arange(4)
+    y = np.array([310, 340, 1160, 1400])
+    # Add error bar on y
+    yerr = np.array([430 - 310, 490 - 340, 2780 - 1160, 4000 - 1400])
+    lolims = np.array([0, 0, 0, 0], dtype=bool)
+    plt.errorbar(x, y, yerr=yerr, fmt='o', color='k', capsize=5, zorder=1, lolims=lolims)
+    plt.xticks([0, 1, 2, 3], ['1', '2', '3', '4'])
+    # draw the bar plot on top of the error bars
+    plt.bar(x, y, color='tab:blue', width=0.5, zorder=2)
+    plt.xlabel('Number of Nw apps')
+    plt.ylabel('Latency (us)')
+    # Make y axis logarithmic and lower value to 0 
+    plt.yscale('log')
+    plt.ylim(bottom=10)
+    plt.yticks([10, 100, 1000, 10000])
+
+def l_app_nw_many_thr():
+    # 19.2
+    # 12.5
+    # 7.4
+    # 5.4
+    x = np.arange(4)
+    y = np.array([19.2, 12.5 * 2, 7.4 * 3, 5.4 * 4])
+    plt.xticks([0, 1, 2, 3], ['1', '2', '3', '4'])
+    plt.bar(x, y, color='tab:blue', width=0.5)
+    plt.xlabel('Number of Nw apps')
+    plt.ylabel('Total throughput (Gbps)')
+
+def t_app_many_nw_one_t_bw():
+    # 750
+    # 701
+    # 181
+    # 145
+    x = np.arange(4)
+    y = np.array([750, 701, 181, 145])
+    plt.xticks([0, 1, 2, 3], ['1', '2', '3', '4'])
+    plt.bar(x, y, color='tab:blue', width=0.5)
+    plt.xlabel('Number of Nw apps')
+    plt.ylabel('Total throughput (MBps)')
+
+def t_app_many_nw_thr():
+    # 18.1	0
+    # 13.6	11.4
+    # 7	7.1
+    # 5.25	5.3
+    x = np.arange(4)
+    y1 = np.array([18.1, 13.6, 7, 5.25])
+    y2 = np.array([0, 11.4, 7.1, 5.3])
+
+    # Plot bar chart of y1 and y2 side-by-side with x as x-axis 
+    plt.bar(x - 0.2, y1, color = 'tab:blue', width = 0.4)
+    plt.bar(x + 0.2, y2, color = 'tab:orange', width = 0.4)
+
+    # Add xticks on the middle of the group bars
+    plt.xlabel('Number of network apps')
+    plt.ylabel('Avg bandwidth (Gbps)')
+    plt.xticks([0, 1, 2, 3], ['1', '2', '3', '4'])
+
+    # Set legend
+    plt.legend(labels=['in-contention', 'non-contention'])
+
+
 if __name__ == '__main__':
     # Set font size 
     plt.rcParams.update({'font.size': 14})
-    l_app_lat()
+    t_app_many_nw_thr()
     plt.show()
